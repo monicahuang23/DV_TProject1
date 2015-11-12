862f132c72300bd421998ee85ce856b6643ff972
Monica Huang <monicahuang@wireless-10-145-200-101.public.utexas.edu>
2015-11-12 10:28:28 -0600

    no message

---
+++ 03 R SQL Visualisations/Crosstab.R
@@ -0,0 +1,43 @@
+require("jsonlite")
+require("RCurl")
+require(ggplot2)
+require(dplyr)
+
+KPI_average = 10000     
+KPI_great = 1000
+
+df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
+"select EMH, DISTRICTNAME, kpi,
+case
+when kpi < "p1" then \\\'Great\\\'
+when kpi < "p2" then \\\'Average\\\'
+else \\\'Ok\\\'
+end kpi
+from (select EMH, DISTRICTNAME, sum(RANK_TOT) as kpi
+  from FINAL_GRADE 
+  group by EMH, DISTRICTNAME)
+order by DISTRICTNAME;"
+')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_great, p2=KPI_average), verbose = TRUE)));View(df)
+
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_discrete() +
+  labs(title='EMH and District Name Crosstab') +
+  labs(x=paste("DISTRICTNAME"), y=paste("EMH")) +
+  layer(data=df, 
+        mapping=aes(x=DISTRICTNAME, y=EMH, label=KPI), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="text",
+        geom_params=list(colour="black"), 
+        position=position_identity()
+  ) +
+  layer(data=df, 
+        mapping=aes(x=DISTRICTNAME, y=EMH, fill=KPI.1), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="tile",
+        geom_params=list(alpha=0.50), 
+        position=position_identity()
+  )
---
+++ 03 R SQL Visualisations/Scatter Plot.R
@@ -0,0 +1,25 @@
+require("jsonlite")
+require("RCurl")
+
+# Change the USER and PASS below to be your UTEid
+df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from final_grade"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+require(extrafont)
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_continuous() +
+  scale_y_continuous() +
+  #facet_wrap(~SURVIVED) +
+  #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
+  #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
+  labs(title='Graduation Rates & Ranks For Each Plantype') +
+  labs(x="SPF P's Grad Rate", y=paste("Rank Total")) +
+  layer(data=df, 
+        mapping=aes(x=as.numeric(as.character(SPF_PS_IND_GRAD_RATE)), y=as.numeric(as.character(RANK_TOT)), color=FINAL_PLANTYPE), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        #position=position_identity()
+        position=position_jitter(width=0.3, height=0)
+  )
